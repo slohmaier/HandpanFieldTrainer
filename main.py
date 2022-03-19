f1c8b8d2638935ba416362b5d6dfbc5d8ba9dd71
Stefan Lohmaier <stefan@slohmaier.de>
2022-03-19 16:14:43 +0100

    working mainwindow

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import sys
 
-from PySide6.QtGui import QGuiApplication
+from PySide6.QtWidgets import QMainWindow, QGridLayout, QLabel, QApplication, QWidget
 
+class HandpanTrainer(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        widget = QWidget()
+        grid = QGridLayout(widget)
+        self.setCentralWidget(widget)
+        grid.addWidget(QLabel('Fields: '))
 
 if __name__ == '__main__':
-    app = QGuiApplication(
+    app = QApplication()
+    
+    widget = HandpanTrainer()
+    widget.show()
+
+    sys.exit(app.exec())
